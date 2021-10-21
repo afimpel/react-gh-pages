3de162ca9b126c9b217d2dff0a72a749287a4dd6
Alvaro Fimpel <afimpel@afimpel.com>
2021-10-21 15:11:15 -0300

    test color and Button

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 import WebIcon from "@mui/icons-material/Web";
 import CloseIcon from "@mui/icons-material/Close";
 //import NotificationsIcon from "@mui/icons-material/Notifications";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import { SnackbarProvider } from "notistack";
 import { Collapse, IconButton } from "@mui/material";
 import ButtonClick from "./Componets/ButtonClick";
@@ -15,17 +16,17 @@ export default function MyForm(props) {
 
   const handleClickWithAction = useCallback(() => {
     enqueueSnackbar("Bienvenidos... afimpel.com", {
-      variant: "default",
+      variant: "success",
       preventDuplicate: true,
       action: (key) => (
         <Fragment>
           <ButtonClick
             color="inherit"
             icons={true}
-            to="//afimpel.com"
+            to="//github.com/afimpel"
             target="_blank"
           >
-            <WebIcon />
+            <GitHubIcon />
           </ButtonClick>
           <IconButton color="inherit" onClick={() => closeSnackbar(key)}>
             <CloseIcon />
@@ -36,15 +37,26 @@ export default function MyForm(props) {
   }, [enqueueSnackbar, closeSnackbar]);
   handleClickWithAction();
   return (
-    <ButtonClick
-      fullWidth={true}
-      to="//afimpel.com"
-      target="_blank"
-      startIcon={<WebIcon />}
-      variant="contained"
-    >
-      afimpel.com
-    </ButtonClick>
+    <Fragment>
+      <ButtonClick
+        color="success"
+        icons={true}
+        to="//github.com/afimpel"
+        target="_blank"
+      >
+        <GitHubIcon />
+      </ButtonClick>
+
+      <ButtonClick
+        color="success"
+        to="//afimpel.com"
+        target="_blank"
+        startIcon={<WebIcon />}
+        variant="contained"
+      >
+        afimpel.com
+      </ButtonClick>
+    </Fragment>
   );
 }
 
